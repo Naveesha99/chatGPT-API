47a3b0122447893b28003e2dfc735a4607198626
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2025-04-29 22:30:56 +0530

    Initial project

---
+++ src/main/java/com/springboot/emailGenerate/EmailGenerateChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.emailGenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailGenerateChatGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailGenerateChatGptApplication.class, args);
+	}
+
+}
